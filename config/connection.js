547fb967e0e24d6b2262674d9929557029dd04f7
Greg Riss <gregriss23@gmail.com>
2021-03-21 14:48:14 -0500

    refactor orm.js, debug connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,19 @@
 const mysql = require('mysql');
 
 let connection;
-if(process.env.JAWSDB_URL && process.env.JAWSDB_URL.length > 0){
+
+if (process.env.JAWSDB_URL && process.env.JAWSDB_URL.length > 0) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "burger_db"
-});
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootroot",
+        database: "burger_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
@@ -23,7 +25,7 @@ connection.connect(function (err) {
 });
 
 // turns on logging
-connection.on('enqueue', function(query) { console.log(query.sql) });
+connection.on('enqueue', function (query) { console.log(query.sql) });
 
 // exporting db connection for the ORM 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,75 +1,42 @@
 // require db connection file
 const connection = require('./connection');
-// function to print questionmarks in generated queries
-function printQuestionMarks(num) {
-    let arr = [];
-    for (let i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
-};
 
-// function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
-    let arr = [];
-
-    // looping through each key, push key/value as string into arr
-    for (let key in ob) {
-        let value = ob[key];
-
-        // checking to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string has spaces, add quotation marks 
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            };
-            arr.push(key + "=" + value);
-        }
-    }
-    // switches array of multiple strings to one comma-separated string
-    return arr.toString();
-}
 // methods that will execute MySQL commands in the controllers; Methods used to retrieve and store data in the database.
-var orm = {
+let orm = {
     selectAll: function (tableInput, cb) {
-        var queryString = "SELECT * FROM ??;";
+        let queryString = "SELECT * FROM ??;";
         connection.query(queryString, [tableInput], function (err, result) {
-            if (err) throw err;
+            if (err) {
+                throw err;
+            }
             cb(result);
         });
     },
-    insertOne: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
-        // I want to change this part...
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString + "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+    insertOne: function (table, values, cb) {
+        let queryString = "INSERT INTO ?? SET ?";
 
         console.log(queryString);
-        connection.query(queryString, vals, function (err, result) {
-            if (err) throw err;
+
+        connection.query(queryString, [table, values], function (err, result) {
+            if (err) {
+                throw err;
+            }
             cb(result);
         })
     },
-    updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE" + table;
-
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+    updateOne: function (table, values, condition, cb) {
+        let queryString = "UPDATE ?? SET ? WHERE ?";
 
         console.log(queryString);
 
-        connection.query(queryString, function(err, result) {
-            if (err) throw err;
+        connection.query(queryString, [table, values, condition], function (err, result) {
+            if (err) {
+                throw err;
+            }
             cb(result);
         });
     }
-}
+};
 
-// Export the ORM object to be used in burger.js (model)
+// Export the ORM object to be used in burger.js model
 module.exports = orm;
