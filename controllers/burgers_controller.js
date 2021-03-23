266e23f10b98f8e153d6b96a1db1791abf618cae
Greg Riss <gregriss23@gmail.com>
2021-03-23 19:05:49 -0400

    debugging PUT route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,26 +4,24 @@ const router = express.Router();
 const burger = require('../models/burger');
 
 // routes 
-router.get("/", (req,res) => {
-    burger.selectAll(function(data) {
+router.get("/", (req, res) => {
+    burger.selectAll(function (data) {
         let hbsObject = { burgers: data };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 // post route
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
     console.log('this is post route');
-    burger.insertOne({burger_name: req.body.burger_name, devoured: req.body.devoured}, function(result) {
-    // send back the ID of new burger
-    console.log(req.body);
-    res.json({ id: result.insertId });
+    burger.insertOne({ burger_name: req.body.burger_name, devoured: req.body.devoured }, function (result) {
+        // send back the ID of new burger
+        console.log(req.body);
+        res.json({ id: result.insertId });
     });
 });
 // put route
-router.put("/api/burgers/:id", function(req, res) {
-    // const burgerId = { id: req.params.id };
-    // const burgerId = `id = ${parseInt(req.params.id)}`;
+router.put("/api/burgers/:id", function (req, res) {
     const burgerId = {
         id: parseInt(req.params.id)
     };
@@ -36,25 +34,26 @@ router.put("/api/burgers/:id", function(req, res) {
         devoured: (req.body.devoured === "true" ? 0 : 1)
     };
     console.log(updateValues);
-    burger.updateOne( updateValues, burgerId, function(result) {
+    burger.updateOne(updateValues, burgerId, function (result) {
 
-        // console.log(result);
-        // devoured: req.body.devoured
-        // if (result.affectedRows == 0) {
-        //     // if no rows changed, then ID doesn't exist ... 404
-        //     return res.status(404).end();
-        // } else {
-        //     res.status(200).end();
-        // }
+        console.log(result);
+
+        if (result.changedRows == 0) {
+            // if no rows changed, then ID doesn't exist ... 404
+            return res.status(404).end();
+        } else {
+            // successful response
+            res.status(200).end();
+        }
     });
 });
-    // delete route
-    router.delete("/api/burger/:id", (req,res) => {
-        burger.delete({ id: req.params.id }, data => {
-            console.log(data);
-            res.json(data);
-        });
+// delete route
+router.delete("/api/burger/:id", (req, res) => {
+    burger.delete({ id: req.params.id }, data => {
+        console.log(data);
+        res.json(data);
     });
+});
 
 // export the router
 module.exports = router;
